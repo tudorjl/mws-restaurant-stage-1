908b23f6a437bfe1f8e2a9f4ae5ef61371dedccc
Joanne Tudor <tudorjl@gmail.com>
2018-05-25 10:06:51 -0400

    chore: Installed eslint ; feature: Added viewport settings in HTML head

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends : "eslint:recommended",
+    plugins: [html],
+    env: {
+        browser: true,
+        es6: true,
+    },
+    rules: {
+    semi: [2,"always"]
+}
+};
